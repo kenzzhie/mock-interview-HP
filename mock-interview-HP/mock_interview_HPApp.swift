920fd2820453466d222f4de3400171fe5389116f
kenzzhie <kenzhiework@gmail.com>
2025-11-24 22:04:41 +0800

    Initial Commit

---
+++ mock-interview-HP/mock_interview_HPApp.swift
@@ -0,0 +1,17 @@
+//
+//  mock_interview_HPApp.swift
+//  mock-interview-HP
+//
+//  Created by Putu A D Kenzhie on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct mock_interview_HPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
